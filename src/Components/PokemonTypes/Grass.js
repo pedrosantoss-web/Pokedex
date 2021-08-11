a6c7179c1994410bc2426ea275cb9ee542552a64
pedrosantoss-web <pedrosantoss167@gmail.com>
2021-08-10 22:06:57 -0300

    Atualizado

---
+++ src/Components/PokemonTypes/Grass.js
@@ -0,0 +1,8 @@
+export default function Grass() {
+  return(
+    <>
+      <h2>Aqui adicionamos as informações dos pokemons do tipo Grass</h2>
+      <p>Mais um desafio legal seria mostrar nessa página todos os pokemons que são do tipo Grass e também aqueles que são bons para lutar contra pokemons de grama. Lembrando que os pokemons do tipo grama são bons contra os pokemons de water, ground e rock. Ou seja: mostrar os que são bons contra eles e os que eles são bons contra.</p>
+    </>
+  )
+}
